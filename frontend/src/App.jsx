e153700e7b473e83e7f784f117afcf2fabae17d4
kaosmoz <maxime.afec@gmail.com>
2026-02-13 13:02:28 +0100

    Sauvegarde avant merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Dashboard from "./Pages/Dashboard";
 import VerifyEmail from "./Pages/VerifyEmail";
-import {Route,Routes} from "react-router-dom";
+import { Route , Routes } from "react-router-dom";
 
 
 function App() {
@@ -12,12 +12,14 @@ function App() {
   return (
     <>
       <Routes>
+
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
 
       </Routes>
+      
     </>
   );
 }
